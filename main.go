2a3c9d83a1eb17d7e64b8e3d3f3d53c78a709f4f
anthony mendoza <e1314213065@live.uleam.edu.ec>
2026-04-10 11:17:15 -0500

    Primer commit - calculadora en Go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+
+import"fmt"
+func main(){
+
+	a :=10
+	b :=5
+	
+	fmt.Printf("Calculadora 00.1")
+
+	fmt.Println("a + b =", a+b)
+	fmt.Println("a - b =",  a-b )
+	fmt.Println("a * b =", a*b)
+	fmt.Printf("a / b = %.2f\n", float64(a)/float64(b))
+
+}
